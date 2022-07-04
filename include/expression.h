4343a754d54635bd3b94a6f164ea4a8d66d12de4
Artem Koton <kotonartem@protonmail.com>
2022-07-04 17:22:46 +0300

    Use unique_ptr for clone (and update tests)

--- include/expression.h
+++ include/expression.h
@@ -3,14 +3,15 @@
 #include "exponential.h"
 
 #include <map>
+#include <memory>
 #include <ostream>
 #include <string>
 
 struct Expression
 {
-    virtual Exponential eval(std::map<std::string, Exponential> const & values = {}) const = 0;
+    virtual Exponential eval(const std::map<std::string, Exponential> & values = {}) const = 0;
 
-    virtual Expression * clone() const = 0;
+    virtual std::unique_ptr<Expression> clone() const = 0;
 
     friend std::ostream & operator<<(std::ostream & out, const Expression & expression);
 };
